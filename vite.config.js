b40fe71573c25c3c7327eeab02c6214d9a016b57
Dorje Lama <dorjelama08@gmail.com>
2024-02-21 22:15:56 +0545

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/portfolio/',
-  plugins: [react()],
-  server: {
-    // Configure MIME types for font files
-    mimeTypes: {
-      // Add or modify the MIME type for TTF files
-      'font/ttf': ['ttf'],
-    },
-  },
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    
+  }
+
+  if (command !== 'serve') {
+    config.base = '/portfolio/'
+  }
+
+  return config
 })
